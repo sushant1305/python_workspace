19729f132b52b42627c1bb886a9bcff8098886ff
sushant1305 <sushant.mad@gmail.com>
2020-07-22 14:50:49 +0530

    Initial commit.

---
+++ sushant.python.test/src/FirstModule.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 18, 2017
+
+@author: Sushant
+'''
+def domain_url(url):
+    url = str(url)
+    print (url)
+
+
+def main():
+    domain_url("http://wwww.youtube.com")
+
+main()
